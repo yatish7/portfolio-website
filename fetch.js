9671aef03de5d766e85c7aa255d67cc735926d36
yatish7 <yatish.manne09@gmail.com>
2024-06-19 23:23:51 +0530

    Minor Changes

--- fetch.js
+++ fetch.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 const GITHUB_USERNAME = process.env.GITHUB_USERNAME;
 const USE_GITHUB_DATA = process.env.USE_GITHUB_DATA;
-const MEDIUM_USERNAME = process.env.MEDIUM_USERNAME;
+/*const MEDIUM_USERNAME = process.env.MEDIUM_USERNAME;*/
 
 const ERR = {
   noUserName:
@@ -94,7 +94,7 @@ if (USE_GITHUB_DATA === "true") {
   req.end();
 }
 
-if (MEDIUM_USERNAME !== undefined) {
+/*if (MEDIUM_USERNAME !== undefined) {
   console.log(`Fetching Medium blogs data for ${MEDIUM_USERNAME}`);
   const options = {
     hostname: "api.rss2json.com",
@@ -128,3 +128,4 @@ if (MEDIUM_USERNAME !== undefined) {
 
   req.end();
 }
+*/
