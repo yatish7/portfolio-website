8a6a0c0a4871f0333a8704a78d7b96f7e80f23a0
yatish7 <yatish.manne09@gmail.com>
2024-06-20 17:08:26 +0530

    Added certifications

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -58,7 +58,7 @@ function Header() {
           )}
           {viewAchievement && (
             <li>
-              <a href="#achievements">Achievements</a>
+              <a href="#achievements">Certifications</a>
             </li>
           )}
           {/*{viewBlog && (
